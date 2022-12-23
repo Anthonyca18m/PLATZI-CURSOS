437f49b5c31ac86889fad63161cbcfa5748641e6
Anthonyca18m <46471376+Anthonyca18m@users.noreply.github.com>
2022-12-23 08:02:44 -0500

    12 - Versión 2_ recurso - Platzi_2022-08-19_14-01-53

--- api/app/Http/Controllers/Api/V2/PostController.php
+++ api/app/Http/Controllers/Api/V2/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V2;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V2\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -21,14 +22,14 @@ class PostController extends Controller
 
     public function show(Post $post)
     {
-        //
+        return new PostResource($post);
     }
 
     public function update(Request $request, Post $post)
     {
         //
     }
-    
+
     public function destroy(Post $post)
     {
         //
