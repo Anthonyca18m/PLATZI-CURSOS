1808e2c8e73f59d2d2a4709ad9215e8d3b9cbf19
Anthonyca18m <46471376+Anthonyca18m@users.noreply.github.com>
2022-12-23 08:42:04 -0500

    14 - Introducción a la API privada - Platzi_2022-08-19_14-02-22

--- api/routes/api.php
+++ api/routes/api.php
@@ -20,6 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 
-Route::apiResource('v1/posts', App\Http\Controllers\Api\V1\PostController::class);
+Route::apiResource('v1/posts', App\Http\Controllers\Api\V1\PostController::class)->middleware('auth:sanctum');
 
-Route::apiResource('v2/posts', App\Http\Controllers\Api\V2\PostController::class);
+Route::apiResource('v2/posts', App\Http\Controllers\Api\V2\PostController::class)->middleware('auth:sanctum');
