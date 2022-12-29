5acdd7dd99ad1fb0180cedbe403e691465f757c5
Anthonyca18m <46471376+Anthonyca18m@users.noreply.github.com>
2022-12-29 12:33:07 -0500

    14 - Creación de la lógica del gráfico 2

--- cashflow/src/components/Resume/Graphic.vue
+++ cashflow/src/components/Resume/Graphic.vue
@@ -7,9 +7,9 @@
                 stroke="#c4c4c4"
                 stroke-width="2"
                 x1="0"
-                y1="100"
+                :y1="zero"
                 x2="300"
-                y2="100"
+                :y2="zero"
             />
             <polyline
                 fill="none"
@@ -42,21 +42,28 @@
 
     const { amounts } = toRefs(props)
 
-    const amountToPixels = () => {
+    const amountToPixels = (amount) => {
         const min = Math.min(...amounts.value)
         const max = Math.max(...amounts.value)
 
-        return `${min}, ${max}`
+        const amountAbs = amount + Math.abs(min)
+        const minMax = Math.abs(max) + Math.abs(min)
+        
+        return 200 - ((amountAbs * 100) / minMax) * 2
     }
 
     const points = computed(() => {
         const total = amounts.value.length
-        return Array(total).fill(100).reduce((points, amount, i) => {
+        return amounts.value.reduce((points, amount, i) => {
             const x = (300 / total) * i
             const y = amountToPixels(amount)
             return `${points} ${x},${y}`
         }, "0, 100")
     })
+
+    const zero = computed(() => {
+        return amountToPixels(0);
+    });
 </script>
 <style>
     
