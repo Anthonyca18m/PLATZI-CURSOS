5f7ae083f451476589000eb0a1d2400a3b90769b
Anthonyca18m <46471376+Anthonyca18m@users.noreply.github.com>
2023-01-06 12:35:36 -0500

    06 - Navegación programática

--- vue-router/src/views/HomeView.vue
+++ vue-router/src/views/HomeView.vue
@@ -1,9 +1,22 @@
-<script setup>
-
-</script>
-
 <template>
-  <main>
-    Hola
-  </main>
+  <div>
+    <label for="">usuario</label>
+    <input v-model="username" type="text" />
+    <button @click="submit">submit</button>
+  </div>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      username: '',
+    }),
+    methods: {
+      submit() {
+        this.$router.push({name: 'chats'}) // ir
+        // this.$router.replace({name: 'chats'}) // reemplazar página sin historial
+        // this.$router.go(-1) // avanzar o retroceder páginas
+      }
+    }
+  }
+</script>
