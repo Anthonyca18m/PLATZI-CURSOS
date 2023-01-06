45cb981ea2633866619c235896f3ac15d9ffd99f
Anthonyca18m <46471376+Anthonyca18m@users.noreply.github.com>
2023-01-06 11:48:31 -0500

    04 - Parámetros y definición de rutas

---
+++ vue-router/src/views/ChatView.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    Chat view
+    <div>
+      {{  this.$route ?? 'undefined'  }}
+
+      <hr>
+      {{  this.$route.params.id ?? ''  }}
+    </div>
+  </div>
+  
+</template>
