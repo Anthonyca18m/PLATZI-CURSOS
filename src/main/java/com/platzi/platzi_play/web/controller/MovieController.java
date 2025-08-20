41094668cb8e8c4cdb8f1be3657f230a4970d3cf
anthonyca18m <anthonyca18m@gmail.com>
2025-08-20 13:26:17 -0500

    Documentación automática de APIs con OpenAPI en Spring Boot

--- src/main/java/com/platzi/platzi_play/web/controller/MovieController.java
+++ src/main/java/com/platzi/platzi_play/web/controller/MovieController.java
@@ -3,6 +3,12 @@ package com.platzi.platzi_play.web.controller;
 import com.platzi.platzi_play.domain.dto.MovieDto;
 import com.platzi.platzi_play.domain.dto.UpdateMovieDto;
 import com.platzi.platzi_play.domain.service.MovieService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/movies")
+@Tag(name = "Movies", description = "Operations related to movies")
 public class MovieController {
 
     private final MovieService service;
@@ -26,7 +33,25 @@ public class MovieController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<MovieDto> getById(@PathVariable Long id) {
+    @Operation (
+            summary = "Get movie by ID",
+            description = "Retrieve a movie by its unique identifier",
+            responses = {
+                @ApiResponse(
+                    responseCode = "200",description = "Movie found", content =
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = MovieDto.class))
+                ),
+                @ApiResponse(responseCode = "404",description = "Movie not found")
+            }
+        )
+    public ResponseEntity<MovieDto> getById(
+            @Parameter(
+                description = "ID of the movie to retrieve",
+                required = true,
+                example = "1"
+            )
+            @PathVariable Long id
+    ) {
         return this.service.getById(id) != null ?
                 ResponseEntity.ok(this.service.getById(id)) :
                 ResponseEntity.notFound().build();
