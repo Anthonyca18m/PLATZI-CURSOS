2e2cb4940efe35983d5b8393974564c796a43cd7
anthonyca18m <anthonyca18m@gmail.com>
2025-08-26 13:46:41 -0500

    creacion de Entities con clave primaria compuesta en JPA

---
+++ src/main/java/platzi/market/persistence/entity/Categoria.java
@@ -0,0 +1,43 @@
+package platzi.market.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Integer id;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "estado")
+    private Boolean estado;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
