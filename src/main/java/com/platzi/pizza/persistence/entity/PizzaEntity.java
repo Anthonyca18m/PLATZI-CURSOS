f69fb0ce27bf20c949aae8e1d672e6ab3973a94b
anthonyca18m <anthonyca18m@gmail.com>
2025-09-09 11:26:50 -0500

    uso de spring data repositories para gestion de datos

---
+++ src/main/java/com/platzi/pizza/persistence/entity/PizzaEntity.java
@@ -0,0 +1,37 @@
+package com.platzi.pizza.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "pizza")
+@Getter
+@Setter
+@NoArgsConstructor
+public class PizzaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pizza", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", unique = true, length = 100)
+    private String name;
+
+    @Column(name = "description", length = 255)
+    private String description;
+
+    @Column(name = "price", nullable = false, columnDefinition = "DECIMAL(5,2)")
+    private Double price;
+
+    @Column(name = "vegetarian", columnDefinition = "TINYINT")
+    private Boolean vegetarian;
+
+    @Column(name = "vegan", columnDefinition = "TINYINT")
+    private Boolean vegan;
+
+    @Column(name = "available", columnDefinition = "TINYINT", nullable = false)
+    private Boolean available;
+}
